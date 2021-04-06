337ddca4ee9b0554a221f8462c66114478f5bfcb
phutong12a9 <phutong12a9@gmail.com>
2021-04-06 16:20:02 +0700

    add chapers 10

---
+++ chapers_10/MailingListRecipien.php
@@ -0,0 +1,14 @@
+<?php
+class MailingListRecipient
+{
+    public $email;
+    public $firstName;
+    public $lastName;
+    
+    public function __construct($email, $firstName, $lastName)
+    {
+        $this->email = $email;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+}
---
+++ chapers_10/json.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'MailingListRecipien.php';
+$recipient = new MailingListRecipient('jdoe@acme.com','John','Doe');
+$requestBody = json_encode($recipient);
+echo $requestBody.PHP_EOL;
